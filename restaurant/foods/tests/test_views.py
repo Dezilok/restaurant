9a7c3e9ae509c363057f2f443bc605dd2cc1fec3
Dezilok <Dezilok@gmail.com>
2021-06-06 19:37:57 +0300

    Test the food forms and update view

--- restaurant/foods/tests/test_views.py
+++ restaurant/foods/tests/test_views.py
@@ -7,13 +7,12 @@ from django.test import RequestFactory
 
 from restaurant.users.models import User
 from ..models import Food
-from ..views import (FoodDetailView, FoodListView, FoodCreateView)
-from .factories import FoodFactory
+from ..views import (FoodDetailView, FoodListView, FoodCreateView, FoodUpdateView)
+from .factories import FoodFactory, food
 
 pytestmark = pytest.mark.django_db
 
 
-#Test for FoodListView
 def test_food_list_view_expanded(rf):
     # Get the request
     request = rf.get(reverse("foods:list"))
@@ -23,10 +22,9 @@ def test_food_list_view_expanded(rf):
     assertContains(response, 'Food List')
 
 
-def test_food_create_view(rf, admin_user):
-    # Order some cheese from the CheeseFactory
-    food = FoodFactory()
-    # Make a request for our new cheese
+def test_food_create_view(rf, admin_user, food):
+    # Order some food from the FoodFactory
+    # Make a request for our new food
     request = rf.get(reverse("foods:add"))
     # Add an authenticated user
     request.user = admin_user
@@ -36,21 +34,20 @@ def test_food_create_view(rf, admin_user):
     assert response.status_code == 200
 
 
-def test_cheese_list_contains_2_foods(rf):
+def test_food_list_contains_2_foods(rf):
     # Create a couple foods
     food1 = FoodFactory()
     food2 = FoodFactory()
     # Create a request and then a response for a list of foods
     request = rf.get(reverse('foods:list'))
     response = FoodListView.as_view()(request)
-    # Assert that the response contains both cheese names in the template.
+    # Assert that the response contains both food names in the template.
     assertContains(response, food1.name)
     assertContains(response, food2.name)
 
 
-def test_detail_contains_food_data(rf):
-    food = FoodFactory()
-    # Make a request for our new cheese
+def test_detail_contains_food_data(rf, food):
+    # Make a request for our new food
     url = reverse("foods:detail", kwargs={'slug': food.slug})
     request = rf.get(url)
     # Use the request to get the response
@@ -65,7 +62,7 @@ def test_detail_contains_food_data(rf):
 
 
 def test_food_create_form_valid(rf, admin_user):
-    # Submit the cheese add form
+    # Submit the food add form
     form_data = {
         "name": "Sweat potato",
         "description": "Sweat sweat potato",
@@ -85,3 +82,38 @@ def test_food_create_form_valid(rf, admin_user):
     assert food.price == 15
 
 
+def test_food_create_correct_title(rf, admin_user):
+    """Page title for FoodCreateView should be Add Food."""
+    request = rf.get(reverse('foods:add'))
+    request.user = admin_user
+    response = FoodCreateView.as_view()(request)
+    assertContains(response, 'Add Food')
+
+
+def test_food_update_view(rf, admin_user, food):
+    url = reverse("foods:update", kwargs={'slug': food.slug})
+    # Make a request for our new food
+    request = rf.get(url)
+    # Add an authenticated user
+    request.user = admin_user
+    # Use the request to get the response
+    callable_obj = FoodUpdateView.as_view()
+    response = callable_obj(request, slug=food.slug)
+    # Test that the response is valid
+    assertContains(response, "Update Food")
+
+
+def test_food_update(rf, admin_user, food):
+    """POST request to FoodUpdateView updates food and redirects."""
+    # Make a request for our new food
+    form_data = {'name': food.name, 'description': 'Something new', 'foodType': food.FoodType, 'price': food.price,
+                 'image': food.image.url}
+    url = reverse("foods:update", kwargs={'slug': food.slug})
+    request = rf.post(url, form_data)
+    request.user = admin_user
+    callable_obj = FoodUpdateView.as_view()
+    response = callable_obj(request, slug=food.slug)
+
+    # # Check that the cheese has been changed
+    food.refresh_from_db()
+    assert food.description == 'Something new'
